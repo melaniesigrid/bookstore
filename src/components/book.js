6ce2e4d4adc96d41e67e9e47a50b1041d39c6f23
Melanie Arellano <melaniesigrid@protonmail.com>
2022-02-25 08:23:28 -0600

    Correct key linter error

--- src/components/book.js
+++ src/components/book.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../App.css';
