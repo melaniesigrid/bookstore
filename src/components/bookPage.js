8338cbce5935af16c36a77ed9ae79ec40c188e02
Melanie Arellano <melaniesigrid@protonmail.com>
2022-02-25 10:14:14 -0600

    Test yet another way for linters

--- src/components/bookPage.js
+++ src/components/bookPage.js
@@ -12,13 +12,15 @@ function BookPage() {
     <div>
       {
         myBooks.map(
-          (book) => (
-            <Book
-              key={book.id}
-              id={book.id}
-              title={book.title}
-              author={book.author}
-              category={book.category} />),
+          (book) =>
+            (
+              <Book
+                key={book.id}
+                id={book.id}
+                title={book.title}
+                author={book.author}
+                category={book.category} />
+            ),
         )
       }
       <AddForm />
