77abddec14e0b3a6818213a5a3af0b415e016318
Melanie Arellano <melaniesigrid@protonmail.com>
2022-02-27 00:00:06 -0600

    Add missing semicolon

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../App.css';
-import profileImage from '../img/user.svg'
+import profileImage from '../img/user.svg';
 
 function Header() {
   return (
